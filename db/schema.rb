86eee5555814fbc4e7ac7f50d4516f07d0ab5acf
shaojunda <shaojunda@gmail.com>
2020-06-01 20:24:45 +0800

    feat: add block_timestamp to udt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_032346) do
+ActiveRecord::Schema.define(version: 2020_06_01_121842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -387,6 +387,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_032346) do
     t.boolean "published", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "block_timestamp", precision: 30
     t.index ["type_hash"], name: "index_udts_on_type_hash", unique: true
   end
 
